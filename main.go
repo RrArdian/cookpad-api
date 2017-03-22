f6e68aec7db78a5da058c91679ff6c4cbadd1783
raisrahman <rais.rahman@reebonz.com>
2017-03-22 14:28:16 +0700

    added detail resep and change server route

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"net/http"
 	"os"
 
+	"strconv"
+
 	"github.com/PuerkitoBio/goquery"
+	"github.com/julienschmidt/httprouter"
 )
 
 type Resep struct {
+	ID          int    `json:"id"`
 	Nama        string `json:"nama"`
 	Ingredients string `json:"ingredients"`
 	Image       string `json:"image"`
@@ -19,7 +23,7 @@ type ResepData struct {
 	Results []Resep `json:"results"`
 }
 
-func ResepHandler(w http.ResponseWriter, r *http.Request) {
+func ResepHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	param := r.URL.Query()
 	keyword := param.Get("q")
 	page := param.Get("page")
@@ -34,14 +38,53 @@ func ResepHandler(w http.ResponseWriter, r *http.Request) {
 
 	var resepData ResepData
 	doc.Find(".recipe").Each(func(index int, item *goquery.Selection) {
+		rid, _ := item.Attr("data-id")
+		id, _ := strconv.Atoi(rid)
 		title := item.Find(".media__body-overflow header span").Text()
 		ingredients := item.Find(".media__body-overflow .recipe__ingredients").Text()
 		img := item.Find("img")
 		image, _ := img.Attr("src")
-		resepData.Results = append(resepData.Results, Resep{Nama: title, Ingredients: ingredients, Image: image})
+		resepData.Results = append(resepData.Results, Resep{ID: id, Nama: title, Ingredients: ingredients, Image: image})
 	})
 
 	js, err := json.Marshal(resepData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+type ResepDetail struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	Author      string `json:"author"`
+}
+
+func ResepDetailHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	doc, err := goquery.NewDocument("https://cookpad.com/id/resep/" + id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var resepDetail ResepDetail
+	doc.Find(".editor").Each(func(index int, item *goquery.Selection) {
+		rid, _ := strconv.Atoi(id)
+		title := item.Find(".intro-container h1").Text()
+		description := item.Find(".recipe-show__story p").Text()
+		img := item.Find(".tofu_image img")
+		image, _ := img.Attr("src")
+		author := item.Find(".media__img span").Text()
+		resepDetail = ResepDetail{ID: rid, Title: title, Description: description, Image: image, Author: author}
+	})
+
+	js, err := json.Marshal(resepDetail)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -57,7 +100,8 @@ func main() {
 		port = "5000"
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/resep", ResepHandler)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	route := httprouter.New()
+	route.GET("/resep", ResepHandler)
+	route.GET("/resep/:id", ResepDetailHandler)
+	log.Fatal(http.ListenAndServe(":"+port, route))
 }
